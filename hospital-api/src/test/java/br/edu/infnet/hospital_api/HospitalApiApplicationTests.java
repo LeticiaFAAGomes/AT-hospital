5b4a4e266b14af4e60e50a84a86351393c3abbfc
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-06-25 17:43:08 -0300

    feat: add estrutura inicial do projeto

---
+++ hospital-api/src/test/java/br/edu/infnet/hospital_api/HospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.hospital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
